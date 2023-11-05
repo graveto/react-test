00334685fdfe48be2e06ae2a6af609b22ccf8336
root <root@DESKTOP-9NPGFSC>
2023-11-04 17:00:49 -0700

    Done

---
+++ src/assets/data/data.js
@@ -0,0 +1,7 @@
+const data = [
+  { id: 1, title: "Project 1", imageURL: "/images/image1.jpeg" },
+  { id: 2, title: "Project 2", imageURL: "/images/image2.jpeg" },
+  { id: 3, title: "Project 3", imageURL: "/images/image3.jpeg" }
+];
+
+export default data;
---
+++ src/components/Projects/index.jsx
@@ -0,0 +1,21 @@
+import data from "../../assets/data/data";
+import image1 from "/images/image1.jpeg";
+
+export default function Projects() {
+  console.log(data);
+  return (
+    <>
+      <div>Hello World</div>
+      <ul>
+        {data.map((img) => {
+          return (
+            <li key={img.id}>
+              <p>{img.title}</p>
+              <img src={img.imageURL} />
+            </li>
+          );
+        })}
+      </ul>
+    </>
+  );
+}
